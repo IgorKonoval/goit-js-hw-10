76a18c3d7c6752a70f089fcd58c861b0942c6df9
Igor Konoval <kip_y@sfa.org.ua>
2023-09-24 23:18:03 +0300

    edit HW

---
+++ src/js/cat-api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_j8r0Cvt7ftCOndWD8NF1pHtGIbcUXctmPnr2b8kQYOKbg7q2N6v3XK848uTkIbwu';
+
+const BASE_URL = `https://api.thecatapi.com/v1/`;
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}breeds`, axios).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+      );
+      throw error;
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
